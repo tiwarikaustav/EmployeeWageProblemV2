b809a627dacca5c29405b0074a6129939c20b4c1
Aryav <tiwarikaustav@gmail.com>
2020-10-04 20:03:05 +0530

    Added the facility for employee to be half day present

--- EmployeeWageProblemV2/Program.cs
+++ EmployeeWageProblemV2/Program.cs
@@ -7,27 +7,43 @@ namespace EmployeeWageProblemV2
         static void Main(string[] args)
         {
             int isPresent = Attendance();
-            int fullDayHour = 8;
-            int wagePerHour = 20;
-            int dailyWage = DailyWage(fullDayHour, wagePerHour);
+            int dailyWage = DailyWage(isPresent);
             if (isPresent == 0)
                 Console.WriteLine("Employee is Absent");
+            else if (isPresent == 1)
+                Console.WriteLine("Employee is present but half day");
             else
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("employee is present full day");
 
             Console.WriteLine("Daily wage of the employee is: {0}", dailyWage);
         }
 
-        //Gives random numbers 0/1, 0=Absent, 1=Present
+        //Gives random numbers 0/1/2, 0=Absent, 1=Half day present, 2=Full day present
         static int Attendance()
         {
             Random randObj = new Random();
-            return randObj.Next(0, 2);
+            return randObj.Next(0, 3);
         }
 
-        static int DailyWage(int fullDayHour, int wagePerHour)
+        //Computes daily wage of employee if he is absent, half day present or full day present
+        static int DailyWage(int flag)
         {
-            return fullDayHour * wagePerHour;
+            int fullDayHour = 8;
+            int halfDayHour = 4;
+            int wagePerHour = 20;
+            if (flag == 0)
+            {
+                return 0;
+            }
+            else if (flag == 1)
+            {
+                return halfDayHour * wagePerHour;
+            }
+            else
+            {
+                return fullDayHour * wagePerHour;
+            }
         }
+
     }
 }
