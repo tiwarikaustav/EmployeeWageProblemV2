913e415cd801eb921dea7fa2294776f2a55322ff
Aryav <tiwarikaustav@gmail.com>
2020-10-04 19:53:42 +0530

    Daily wage of employee is computed

--- EmployeeWageProblemV2/Program.cs
+++ EmployeeWageProblemV2/Program.cs
@@ -6,7 +6,28 @@ namespace EmployeeWageProblemV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int isPresent = Attendance();
+            int fullDayHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = DailyWage(fullDayHour, wagePerHour);
+            if (isPresent == 0)
+                Console.WriteLine("Employee is Absent");
+            else
+                Console.WriteLine("Employee is Present");
+
+            Console.WriteLine("Daily wage of the employee is: {0}", dailyWage);
+        }
+
+        //Gives random numbers 0/1, 0=Absent, 1=Present
+        static int Attendance()
+        {
+            Random randObj = new Random();
+            return randObj.Next(0, 2);
+        }
+
+        static int DailyWage(int fullDayHour, int wagePerHour)
+        {
+            return fullDayHour * wagePerHour;
         }
     }
 }
